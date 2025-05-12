86e9d33ce8732d3db69a551047bb1b02e16d7875
Aaradhyaghota <aaradhyaghota@gmail.com>
2025-05-13 00:03:04 +0530

    added script to run the multiple command at once

---
+++ build_images.py
@@ -0,0 +1,18 @@
+import os
+
+commands = [
+    "docker build -t aaradhyaghota/middleware:latest /home/aaradhya-ghota/SoftwareProduction/Major_Project_spe/Backend/MiddleWare",
+    "docker build -t aaradhyaghota/model-service:latest /home/aaradhya-ghota/SoftwareProduction/Major_Project_spe/Backend/Model-service",
+    "docker build -t aaradhyaghota/frontend:latest /home/aaradhya-ghota/SoftwareProduction/Major_Project_spe/frontend",
+    "docker push aaradhyaghota/middleware:latest",
+    "docker push aaradhyaghota/model-service:latest",
+    "docker push aaradhyaghota/frontend:latest",
+    "kubectl apply -f /home/aaradhya-ghota/SoftwareProduction/Major_Project_spe/Backend/Kubernates/mysql.yaml",
+    "kubectl apply -f /home/aaradhya-ghota/SoftwareProduction/Major_Project_spe/Backend/Kubernates/model-service.yaml",
+    "kubectl apply -f /home/aaradhya-ghota/SoftwareProduction/Major_Project_spe/Backend/Kubernates/middleware.yaml",
+    "kubectl apply -f /home/aaradhya-ghota/SoftwareProduction/Major_Project_spe/frontend/frontend.yaml"
+]
+
+for cmd in commands:
+    print(f"\nRunning: {cmd}")
+    os.system(cmd)
