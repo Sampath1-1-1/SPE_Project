6ee9570783e8f14394b5827cc6c6a2f9edfcc53a
Aaradhyaghota <aaradhyaghota@gmail.com>
2025-05-17 16:55:54 +0530

    code for testing

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestApplication(unittest.TestCase):
+    def test_placeholder(self):
+        # Placeholder test to simulate automated testing
+        self.assertTrue(True, "This is a placeholder test")
+
+if __name__ == '__main__':
+    unittest.main()
